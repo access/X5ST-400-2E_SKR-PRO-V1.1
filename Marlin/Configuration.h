375e786b62cd79d588cd083be90c6275f9573da3
access <tommi_versetti@list.ru>
2021-01-01 12:38:05 +0200

    Z sensitivy 8 to 60

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -454,7 +454,7 @@
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
 #define HEATER_5_MAXTEMP 275
-#define BED_MAXTEMP      120
+#define BED_MAXTEMP      125
 
 //===========================================================================
 //============================= PID Settings ================================
